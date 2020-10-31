c7fe7dbd64b4dee80aa2a539bb48fa7d6544cb40
meggs1 <megan.schertz@gmail.com>
2020-10-31 13:19:23 -0500

    commit tree c749241927a7406460a9d84d2c9a6e56a26af502

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+require 'pry'
+
+def prime?(i)
+    if i <= 1
+        return false
+    elsif
+        (2..i- 1).each do |x| #creates a range of 2 to the number before any integer
+            if i % x == 0    
+                return false
+            end
+        end  
+    end
+    return true
+end
